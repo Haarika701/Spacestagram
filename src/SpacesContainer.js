ebdb9918f85e02fe8cd568e4ae33df8616ada214
HaarikaR <hramadugu@gmail.com>
2022-01-15 11:32:17 -0800

    loading state added....

--- src/SpacesContainer.js
+++ src/SpacesContainer.js
@@ -4,15 +4,21 @@ import SpacesView from "./SpacesView"
 export default function SpacesContainer(){
 
     const NASA_BASE_URL = `https://api.nasa.gov/planetary/apod?api_key=0JqA3znQkwnAI7ImklsVVD9i0lfmbebahzsyu84z&start_date=2021-09-01&end_date=2021-09-30`
+     
 
 
     const[allSpaces,setAllSpaces] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
     fetch(NASA_BASE_URL)
     .then(resp => resp.json())
     .then(data => setAllSpaces(data))
+    setIsLoading(false)
+
+    if (isLoading) return <div>Page is loading</div>
     return (
         <div>
-            Hi!
+            <h1>Spacestagram</h1>
             <SpacesView allSpaces = {allSpaces}/>
         </div>
     )
